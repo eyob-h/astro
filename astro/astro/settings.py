04d20bfbf653ca630bde6aa5afdc033b6cbf4f6f
Eyob <110186516+eyob-h@users.noreply.github.com>
2023-05-19 19:39:33 +0300

    create admin

--- astro/astro/settings.py
+++ astro/astro/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^g98e6+6=lnahx0c@y%=6$ie=_u$7g^u_p-c3c!nsxis-d@wev'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
